c28e9a12f2e00bbad40a163ec16364be13917573
Gudipati <vamshi.gudipati@metlife.com>
2017-01-03 13:17:04 -0500

    Finished building the app, integrated the compiler

--- src/App.js
+++ src/App.js
@@ -3,16 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+    constructor(){
+      super();
+      this.state = {
+        input: "Enter your JSX here",
+        output: '',
+        err: ''
+      }
+    }
+    update(e){
+  let code = e.target.value;
+  try {
+    this.setState({
+      output: window.Babel
+      .transform(code, { presets: ['es2015', 'react']})
+      .code,
+      err: ''
+    })
+  }
+  catch(err){
+    this.setState({err: err.message})
+  }
+}
+    render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to JSX Transpiler with React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header> {this.state.err} </header>
+        <div className="container">
+        <textarea
+        onChange={this.update.bind(this)}
+        defaultValue={this.state.input}>
+        </textarea>
+        <pre>{this.state.output}</pre>
+        </div>
       </div>
     );
   }
